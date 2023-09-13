9e78850901b8a8dbf71a022a2d5433d1a657e6c0
pfaffrob <104759860+pfaffrob@users.noreply.github.com>
2023-09-14 00:56:20 +0200

    Calculations and plots

--- code/my_functions.py
+++ code/my_functions.py
@@ -1,4 +1,6 @@
 import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
 import os
 import _osx_support
 import rasterio
@@ -263,7 +265,27 @@ def reclassify(input_file, output_file, reclassify_values):
         with rasterio.open(output_file, "w", **meta) as dest:
             dest.write(reclassified_raster.astype(rasterio.int32), 1)
             
-                     
+            
+def replace_nan_with_zero(input_file, output_file):
+    with rasterio.open(input_file) as src:
+        img = src.read(1)  # read the first band
+
+        # Replace NaN values with 0
+        img = np.where(np.isnan(img), 0, img)
+
+        # Define the profile for the output file
+        profile = src.profile
+        profile.update(
+            dtype=rasterio.float32,
+            count=1,
+            compress='lzw'
+        )
+
+        # Write the output file
+        with rasterio.open(output_file, 'w', **profile) as dst:
+            dst.write(img.astype(rasterio.float32), 1)     
+            
+                            
 def mask_tif_select_nodata(input_tif, mask_tif, output_tif, nodata_value=[0]):
     with rasterio.open(input_tif) as src:
         with rasterio.open(mask_tif) as mask:
@@ -300,9 +322,29 @@ def mask_tif_with_shapefile(input_tif, shapefile, output_tif, nodata_value=None)
             nodata_value = 0
         out_image[:, ~mask] = nodata_value
         out_meta = src.meta
-        out_meta.update({"nodata": nodata_value})
+        out_meta.update({"nodata": nodata_value, "compress": "lzw"})  # Add compression setting
         with rasterio.open(output_tif, "w", **out_meta) as dest:
-            dest.write(out_image)               
+            dest.write(out_image)      
+
+def mask_tif_with_shapefile_select_feature_value(input_tif, shapefile, output_tif, nodata_value=None, feature_name=None, feature_value=None):
+    with rasterio.open(input_tif) as src:
+        shapes = gpd.read_file(shapefile)
+
+        # Check if both feature_name and feature_value are provided
+        if feature_name is not None and feature_value is not None:
+            shapes = shapes[shapes[feature_name] == feature_value]
+
+        mask = geometry_mask(shapes.geometry, out_shape=src.shape, transform=src.transform, invert=True)
+        out_image = src.read()
+        if nodata_value is None:
+            nodata_value = src.nodata
+        if nodata_value is None:
+            nodata_value = 0
+        out_image[:, ~mask] = nodata_value
+        out_meta = src.meta
+        out_meta.update({"nodata": nodata_value, "compress": "lzw"})  # Add compression setting
+        with rasterio.open(output_tif, "w", **out_meta) as dest:
+            dest.write(out_image)
 
 
 def mask_tif_select_value(input_tif, mask_tif, output_tif, mask_values, inside=False, nodata_value=None):
@@ -323,9 +365,30 @@ def mask_tif_select_value(input_tif, mask_tif, output_tif, mask_values, inside=F
             else:
                 out_image[mask] = nodata_value
             out_meta = src.meta
-            out_meta.update({"nodata": nodata_value})
+            out_meta.update({"nodata": nodata_value, "compress": "lzw"})
             with rasterio.open(output_tif, "w", **out_meta) as dest:
-                dest.write(out_image)           
+                dest.write(out_image)
+   
+def mask_tif_larger_smaller_value(input_tif, mask_tif, output_tif, is_greater=True, nodata_value=None):
+    with rasterio.open(input_tif) as src_input:
+        with rasterio.open(mask_tif) as src_mask:
+            input_data = src_input.read()
+            mask_data = src_mask.read()
+            if nodata_value is None:
+                nodata_value = src_input.nodata
+            if nodata_value is None:
+                nodata_value = src_mask.nodata  # Use nodata value from mask TIFF
+            if nodata_value is None:
+                nodata_value = 0
+            if is_greater:
+                mask_condition = (mask_data > input_data) | (mask_data == 0)
+            else:
+                mask_condition = (mask_data < input_data) | (mask_data == 0)
+            output_data = np.where(mask_condition, nodata_value, input_data)
+            out_meta = src_input.meta
+            out_meta.update({"nodata": nodata_value, "compress": "lzw"})
+            with rasterio.open(output_tif, "w", **out_meta) as dest:
+                dest.write(output_data)
             
                 
 def get_pixel_size(filename):
@@ -421,3 +484,92 @@ def find_tif_files_with_polygon(root_folder, gdf_boundary, keyword, minimum_area
                     if np.any(np.where(mask, raster > 0, False)):
                         tif_files.append(tif_path)
     return tif_files
+
+
+def create_high_res_tif_from_mask(input_tif, mask_tif, output_tif):
+    with rasterio.open(input_tif) as src:
+        input_data = src.read(1)
+        input_transform = src.transform
+        input_crs = src.crs
+
+    with rasterio.open(mask_tif) as msk:
+        mask_data = msk.read(1)
+        mask_transform = msk.transform
+
+    # Calculate the scaling factors between the two rasters
+    scale_x = int(np.ceil(input_transform[0] / mask_transform[0]))
+    scale_y = int(np.ceil(input_transform[4] / mask_transform[4]))
+
+    # Calculate the percentage of pixels with value 1 in each region
+    region_sums = np.add.reduceat(np.add.reduceat(mask_data, np.arange(0, mask_data.shape[0], scale_y), axis=0),
+                                  np.arange(0, mask_data.shape[1], scale_x), axis=1)
+    percentages = region_sums / (scale_x * scale_y)
+
+    # Pad percentages array with ones if necessary
+    pad_y = input_data.shape[0] - percentages.shape[0]
+    pad_x = input_data.shape[1] - percentages.shape[1]
+    if pad_y > 0 or pad_x > 0:
+        percentages = np.pad(percentages, ((0, pad_y), (0, pad_x)), constant_values=1)
+
+    # Calculate the new values for each region
+    new_values = np.nan_to_num(input_data / percentages)
+
+    # Assign these values to the corresponding pixels in the output raster
+    output_data = new_values.repeat(scale_y, axis=0).repeat(scale_x, axis=1) * (mask_data == 1)
+
+    # Write the output raster
+    with rasterio.open(output_tif, 'w', driver='GTiff', height=output_data.shape[0],
+                       width=output_data.shape[1], count=1, dtype=str(output_data.dtype),
+                       crs=input_crs, transform=mask_transform, compress='lzw') as dst:
+        dst.write(output_data, 1)
+
+
+
+def process_raster_to_csv_and_plot(title, input_tif, output_csv, output_png, included_values=None, show_sum=False):
+    with rasterio.open(input_tif) as src:
+        spatial_resolution = src.res[0]
+        conversion_factor = (spatial_resolution ** 2) / 10000
+
+        band1 = src.read(1)
+        unique, counts = np.unique(band1, return_counts=True)
+        
+        if included_values is not None:
+            included_vals = np.asarray(included_values)
+            unique = np.intersect1d(unique, included_vals)
+        
+        data = dict(zip(unique[unique != 0], counts[unique != 0] * conversion_factor))
+        df = pd.DataFrame(list(data.items()), columns=['Year', 'Area (ha)'])
+        df['Year'] = df['Year']
+        df.to_csv(output_csv, index=False)
+    
+    total_area = df['Area (ha)'].sum()
+    plt.style.use('bmh')
+    df = pd.read_csv(output_csv)
+
+    ax = df.plot(x='Year', y='Area (ha)', kind='bar')
+    
+    if show_sum:
+        total_area_rounded = round(total_area)
+        for i in ax.containers:
+            ax.bar_label(i, label_type='edge', rotation=0 if len(df) <= 5 else 90, padding=5, fmt=lambda x: f'{x:.0f}')
+        
+        ax.text(0.5, 1.03 if len(df) <= 5 else 1.05, f'Total Area: {total_area_rounded} ha', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize=12, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round'))
+
+    else:  # When show_sum is False, still show bar labels
+        for i in ax.containers:
+            ax.bar_label(i, label_type='edge', rotation=0 if len(df) <= 5 else 90, padding=5, fmt=lambda x: f'{x:.0f}')
+    
+    ax.spines['bottom'].set_visible(True)
+    ax.xaxis.grid(False)
+    ax.yaxis.grid(True)
+
+    y_max = df['Area (ha)'].max()
+    ax.set_ylim(0, y_max * 1.15)
+
+    # Adjust the vertical position of the title and box if show_sum is True
+    title_pad = 50 if show_sum and len(df) > 5 else 30  # Adjust the value as needed
+    ax.set_title(title, pad=title_pad)
+
+    plt.savefig(output_png, bbox_inches='tight')
+
+
